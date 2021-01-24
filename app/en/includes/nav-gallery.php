a2818af183731202888f7266b3e33aa680b32e21
gnmg <ralph@ralphreene.info>
2021-01-25 00:32:07 +0900

    gallery

--- app/en/includes/nav-gallery.php
+++ app/en/includes/nav-gallery.php
@@ -20,10 +20,10 @@
           <li class="dropdown">
             <a data-toggle="dropdown" href="#">WINNERS GALLERY<span class="caret"></span></a></a>
             <ul class="dropdown-menu">
-              <li><a href="../en/gallery/index.php">2020 Gallery</a></li>
-              <li><a href="../en/gallery/gallery-2019.php">2019 Gallery</a></li>
-              <li><a href="../en/gallery/gallery-2018.php">2018 Gallery</a></li>
-              <li><a href="../en/gallery/gallery-2017.php">2017 Gallery</a></li>
+              <li><a href="../../en/gallery/index.php">2020 Gallery</a></li>
+              <li><a href="../../en/gallery/gallery-2019.php">2019 Gallery</a></li>
+              <li><a href="../../en/gallery/gallery-2018.php">2018 Gallery</a></li>
+              <li><a href="../../en/gallery/gallery-2017.php">2017 Gallery</a></li>
             </ul>
           </li>
           <li class="dropdown">
