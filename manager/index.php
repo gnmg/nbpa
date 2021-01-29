fed2fa86ae474660d711f3a4857e83a3f9ecd6ed
gnmg <ralph@ralphreene.info>
2021-01-29 09:01:12 +0900

    change folder name

---
+++ manager/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Entry point
+ *
+ * @version SVN: $Id$
+ */
+define('APP_ROOT', __DIR__.'/../nbpa_sys2021');
+
+require_once APP_ROOT.'/vendor/autoload.php';
+require_once APP_ROOT.'/app/bootstrap_manager.php';
+require_once APP_ROOT.'/app/dispatcher_manager.php';
+
+$app->run();
