84d54bc2296a3074af22b4b2bd65ab58eb67044b
gnmg <ralph@ralphreene.info>
2021-01-29 09:23:27 +0900

    remove links

--- nbpa_sys2021/app/NBPA/Templates/en/partials/sys_template_footer.php
+++ nbpa_sys2021/app/NBPA/Templates/en/partials/sys_template_footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
   <div class="footer__top"><a href="" onclick="return false"><img src="/assets/images/common__pagetop.png" alt="go to top" /></a></div>
-  <div class="footer__sns">
+  <!-- <div class="footer__sns">
     <div class="inner-container">
       <nav>
         <ul>
@@ -15,7 +15,7 @@
         </ul>
       </nav>
     </div>
-  </div>
+  </div> -->
   <div class="footer__links">
     <div class="inner-container">
       <nav>
@@ -37,14 +37,14 @@
         </ul>
         <div class="ssl">
           <!--- DO NOT EDIT - GlobalSign SSL Site Seal Code - DO NOT EDIT --->
-          <table style="margin:0 auto 35px;" width=125 border=0 cellspacing=0 cellpadding=0 title="CLICK TO VERIFY: This site uses a GlobalSign SSL Certificate to secure your personal information.">
+          <!-- <table style="margin:0 auto 35px;" width=125 border=0 cellspacing=0 cellpadding=0 title="CLICK TO VERIFY: This site uses a GlobalSign SSL Certificate to secure your personal information.">
             <tr>
               <td><span id="ss_img_wrapper_gmogs_image_110-45_en_white"><a href="https://www.globalsign.com/" target=_blank
                     title="GlobalSign Site Seal" rel="nofollow"><img alt="SSL" border=0 id="ss_img" src="//seal.globalsign.com/SiteSeal/images/gs_noscript_110-45_en.gif"></a></span>
                 <script type="text/javascript" src="//seal.globalsign.com/SiteSeal/gmogs_image_110-45_en_white.js"></script>
               </td>
             </tr>
-          </table>
+          </table> -->
           <!--- DO NOT EDIT - GlobalSign SSL Site Seal Code - DO NOT EDIT --->
 
 
