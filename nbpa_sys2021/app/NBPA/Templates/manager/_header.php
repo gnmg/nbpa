b451cf8f0e507813962bce51abf7fa402f030d8c
gnmg <ralph@ralphreene.info>
2021-02-21 23:28:30 +0900

    change composer

--- nbpa_sys2021/app/NBPA/Templates/manager/_header.php
+++ nbpa_sys2021/app/NBPA/Templates/manager/_header.php
@@ -1,8 +1,7 @@
 <!doctype html>
-<html ng-app>
+<html lang="en">
 
   <head>
-  <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
   <meta content="utf-8" http-equiv="encoding">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
