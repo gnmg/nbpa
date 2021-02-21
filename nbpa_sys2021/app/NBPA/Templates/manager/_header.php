546c83bcc218479fdaa957f834d849dba6e4c4c2
gnmg <ralph@ralphreene.info>
2021-02-21 23:06:28 +0900

    change html

--- nbpa_sys2021/app/NBPA/Templates/manager/_header.php
+++ nbpa_sys2021/app/NBPA/Templates/manager/_header.php
@@ -2,6 +2,8 @@
 <html ng-app>
 
   <head>
+  <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+  <meta content="utf-8" http-equiv="encoding">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NBPA Management Console</title>
