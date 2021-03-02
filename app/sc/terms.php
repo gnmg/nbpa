59e9dff936b13d686929ccdadc65dfbac1d346f4
gnmg <ralph@ralphreene.info>
2021-03-02 14:27:20 +0900

    add footer to terms

--- app/sc/terms.php
+++ app/sc/terms.php
@@ -14,13 +14,10 @@
                 <h2><img src="../../assets/images/page__terms.png" alt="Term of Use" class="mxw100"></h2>
                 <p>
                     <h3>使用规定</h3>
-                    <p class="class=" text-justify ">登录Nature’s Best Photography Asia网站,浏览该网页时,视为接受本规定的所有内容。如不认可和接受本规定，请停止使用Nature’s Best Photography Asia网站。</p><h3>著作权及商标的使用限制</h3><p class="text-justify
-                        ">Nature’s Best Photography Asia网站上发布的所有信息，受各国的著作权法、相关条约及其他法律保护。除本规定及网站的预先通知许可的情形外，未经著作权所有人书面许可，著作权法等法律、法规禁止非商业性、个人目的及由其他著作权认可外的使用行为(含复制、修改、上传、提示、转发、发布、许可证、销售、出版等)。需要使用时，请提前与Nature’s Best Photography Asia联系，并获得书面许可。Nature’s Best Photography Asia网站上出现的各种商号、商标、标志、产品名称以及服务标志等相关权利，归属于Nature’s Best Photography Asia或各权利所有人。除商标法及其他法律规定的情形外，未经许可，禁止使用。</p><h3>关于免责</h3><p class="text-justify
-                        ">Nature’s Best Photography Asia不能保证内容及网站上发布的所有信息的正确性、实时性及对用户的有效性。并且，因用户登录Nature’s Best Photography Asia网站，发生计算机、关联设备、网络环境、硬件及软件的故障，并由此对用户产生直接或间接损失时，除法律命令的要求情形外，Nature’s Best Photography Asia不负担任何责任。Nature’s Best Photography Asia对网站的构成、使用条件、URL以及内容等，未经预告会进行变更或中止。</p><h3>IP地址、Cookie以及网站信标的使用</h3><p class="text-justify
-                        ">在NBPA上被记录下来的IP地址，可能会用于查找服务器发生问题的原因以及网站管理。NBPA网站使用Cookie是为了用户再次登录的方便，不用于获取用户的个人信息。此外请注意，通过变更使用浏览器的设定，使Cookie失效时，可能会造成不能使用本网站内的各项内容。并且，为了调查用户的使用情况，网站会使用网站信标。网站信标用于收集浏览信息，作为统计信息用于改善网站。网站信标取得的信息，不含用户个人任何特定的信息。</p><h3>其他</h3><p class="text-justify ">使用规定的修订<br>  Nature’s Best Photography Asia会在未经预告的情况下对本规定进行更改，同时更新上传修订版。请确认最新内容。<br> 关于依据的法律条款和管辖权 <br>使用本网站以及本规定的解释以及适用，除有其他规定外，以日本国法律为依据。并且，使用本网站出现的所有争议，除有其他规定外，管辖权归属于东京地方裁判所第一审的专属管辖裁判所。</p></p>
+                    <p class="class="text-justify ">登录Nature’s Best Photography Asia网站,浏览该网页时,视为接受本规定的所有内容。如不认可和接受本规定，请停止使用Nature’s Best Photography Asia网站。</p><h3>著作权及商标的使用限制</h3><p class="text-justify ">Nature’s Best Photography Asia网站上发布的所有信息，受各国的著作权法、相关条约及其他法律保护。除本规定及网站的预先通知许可的情形外，未经著作权所有人书面许可，著作权法等法律、法规禁止非商业性、个人目的及由其他著作权认可外的使用行为(含复制、修改、上传、提示、转发、发布、许可证、销售、出版等)。需要使用时，请提前与Nature’s Best Photography Asia联系，并获得书面许可。Nature’s Best Photography Asia网站上出现的各种商号、商标、标志、产品名称以及服务标志等相关权利，归属于Nature’s Best Photography Asia或各权利所有人。除商标法及其他法律规定的情形外，未经许可，禁止使用。</p><h3>关于免责</h3><p class="text-justify ">Nature’s Best Photography Asia不能保证内容及网站上发布的所有信息的正确性、实时性及对用户的有效性。并且，因用户登录Nature’s Best Photography Asia网站，发生计算机、关联设备、网络环境、硬件及软件的故障，并由此对用户产生直接或间接损失时，除法律命令的要求情形外，Nature’s Best Photography Asia不负担任何责任。Nature’s Best Photography Asia对网站的构成、使用条件、URL以及内容等，未经预告会进行变更或中止。</p><h3>IP地址、Cookie以及网站信标的使用</h3><p class="text-justify ">在NBPA上被记录下来的IP地址，可能会用于查找服务器发生问题的原因以及网站管理。NBPA网站使用Cookie是为了用户再次登录的方便，不用于获取用户的个人信息。此外请注意，通过变更使用浏览器的设定，使Cookie失效时，可能会造成不能使用本网站内的各项内容。并且，为了调查用户的使用情况，网站会使用网站信标。网站信标用于收集浏览信息，作为统计信息用于改善网站。网站信标取得的信息，不含用户个人任何特定的信息。</p><h3>其他</h3><p class="text-justify ">使用规定的修订<br>  Nature’s Best Photography Asia会在未经预告的情况下对本规定进行更改，同时更新上传修订版。请确认最新内容。<br> 关于依据的法律条款和管辖权 <br>使用本网站以及本规定的解释以及适用，除有其他规定外，以日本国法律为依据。并且，使用本网站出现的所有争议，除有其他规定外，管辖权归属于东京地方裁判所第一审的专属管辖裁判所。</p></p>
         </div>
       </div>
-      <?php include "includes/footer.php ";?>
+      <?php include "includes/footer.php";?>
   
 </body>
 </html>
