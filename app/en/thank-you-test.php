d71526938aca1a085abee1e6c5b854ea12d3624a
gnmg <ralph@ralphreene.info>
2021-03-16 12:14:56 +0900

    thanks page changes

---
+++ app/en/thank-you-test.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "includes/head.php";?>
+
+<body data-spy="scroll" data-target=".onpage-navigation" data-offset="150">
+    <!--<script type="text/javascript" src="https://app.getresponse.com/view_webform_v2.js?u=BKM8I&webforms_id=9131504"></script>-->
+    <div id="wrap" class="en">
+        <?php include "includes/nav.php";?>
+        <div class="container-fluid document-page">
+            <div class="container details__contact">
+                <div class="col-md-12">
+                    <div class="col-md-10 col-md-offset-1">
+                        <div class="col-md-12 ">
+                            <p><h3>Thank you. Your payment was successful.</h3></p>
+                            <p><h3><a style="color:red"href="https://naturesbestphotography.asia/user/member/home">Please click here to go
+                                    back to your account page and upload photos.</a><h3></p>
+                            <p style="color:blue">If you do not see the upload button then please reload
+                                the browser page.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include "includes/footer.php";?>
+
+</body>
+
+</html>
---
+++ app/ja/thank-you-test.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "includes/head.php";?>
+
+<body data-spy="scroll" data-target=".onpage-navigation" data-offset="150">
+    <!--<script type="text/javascript" src="https://app.getresponse.com/view_webform_v2.js?u=BKM8I&webforms_id=9131504"></script>-->
+    <div id="wrap" class="en">
+        <?php include "includes/nav.php";?>
+        <div class="container-fluid document-page">
+            <div class="container details__contact">
+                <div class="col-md-12">
+                    <div class="col-md-10 col-md-offset-1">
+                        <div class="col-md-12 ">
+                            <p><h3>ありがとうございました。 お支払いが完了しました。</h3></p>
+                            <p><h3><a style="color:red"href="https://naturesbestphotography.asia/user/member/home">ここをクリックするとマイページに戻り写真のアップロードができます。</a><h3></p>
+                            <p style="color:blue">もしアップロードのボタンが出ていない場合は、ページの再読み込み（リロード）をしてください。</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include "includes/footer.php";?>
+
+</body>
+
+</html>
