577bf9e7d023f819a349c165fe6a93156a0ad4bf
gnmg <ralph@ralphreene.info>
2021-03-18 09:04:33 +0900

    style changes

--- nbpa_sys2021/app/NBPA/Templates/en/member/signup_complete.php
+++ nbpa_sys2021/app/NBPA/Templates/en/member/signup_complete.php
@@ -7,7 +7,10 @@
         <div id="content">
             <h1 class="mainTi">Completion｜Registration</h1>
             <div id="regist_form">
-                <p class="confirm_red">Please check your e-mail box and confirm your registration.</p>
+                <p class="confirm_red">Please check your e-mail and confirm your registration.</p>
+                <p class="confirm_red">If you do not see the email from us then please check your spam forlder.</p>
+                <p class="confirm_red">If the email still has not arrived then please send a message to the following address and we will help you.</p>
+                <p style="color:blue;"><a href='mailto: support@naturesbestphotography.asia'>support@naturesbestphotography.asia</a></p>
 
             </div><!-- regist_form END -->
         </div><!-- contant END -->
