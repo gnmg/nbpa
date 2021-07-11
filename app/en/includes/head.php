f08beef494831960ed0e7fb029576046931561c4
gnmg <ralph@ralphreene.info>
2021-07-11 22:13:10 +0900

    add tawk.to

--- app/en/includes/head.php
+++ app/en/includes/head.php
@@ -40,5 +40,18 @@
    <link rel="stylesheet" type="text/css" href="../../../assets/styles/style2.css" media="all">
 
    <script type="text/javascript" src="../../../assets/styles/jquery_lazy/jquery.lazy.min.js"></script>
+   <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60eae83ad6e7610a49aab747/1faapm9q1';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </head>
 
